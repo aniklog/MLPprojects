52b481f08fcea30293a868bced512d21f60d08e2
MounikaKolisetty <56874374+MounikaKolisetty@users.noreply.github.com>
2021-04-08 21:49:51 +0200

    Changes Made

--- Project/GUI.py
+++ Project/GUI.py
@@ -6,7 +6,9 @@ import pandas as pd
 from scipy import interp
 from itertools import cycle
 import matplotlib.pyplot as plt
+from tkinter import *
 from tkinter import messagebox
+from tkinter import filedialog
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
@@ -21,33 +23,42 @@ root.title("Discrimination of Reflected Sound Signal(MLP Classifier)")
 
 # setting canvas size and grid
 canvas = tk.Canvas(root, width = 1000, height = 1000)
-canvas.grid(columnspan = 6, rowspan = 12)
+canvas.grid(rowspan = 14, columnspan = 15)
 
 #For Training
 tk.Label(root, text = "Training the Model", font = 'customFont1', fg = "black", bg = "sky blue", width = 15).grid(columnspan = 1, row = 0, column = 0)
 
-folder_path = tk.StringVar()    
+def browse_folder() :
+    global folder_path
+    filename = filedialog.askdirectory()
+    folder_path.set(filename)
+
+folder_path = tk.StringVar()
 tk.Label(root, text = "Path for Training the Model : ", font = 'customFont1', fg = "black", width = 20).grid(columnspan = 1, row = 1, column = 0)
 training_path = tk.Entry(root, width = 60, textvariable = folder_path).grid(row = 1, column = 1)
 
+browseBtn = tk.Button(root, text = "Browse Folder", command = lambda:browse_folder(), font = 'customFont1', bg = "azure", fg = "black", height = 2, width = 15)
+browseBtn.grid(row = 1, column = 2)
+
 start_column = tk.IntVar()
-tk.Label(root, text = "Signal Starts at Column : ", font = 'customFont1', fg = "black", width = 20).grid(columnspan = 1, row = 1, column = 2)
-signal_start = tk.Entry(root, width = 5, textvariable = start_column).grid(row = 1, column = 3)  
+tk.Label(root, text = "Signal Starts at Column : ", font = 'customFont1', fg = "black", width = 20).grid(columnspan = 1, row = 2, column = 0)
+signal_start = tk.Entry(root, width = 5, textvariable = start_column).grid(row = 2, column = 1, sticky = "W")
 
 end_column = tk.IntVar()
-tk.Label(root, text = "Signal Ends at Column : ", font = 'customFont1', fg = "black", width = 20).grid(columnspan = 1, row = 1, column = 4)
-signal_start = tk.Entry(root, width = 5, textvariable = end_column).grid(row = 1, column = 5)
+tk.Label(root, text = "Signal Ends at Column : ", font = 'customFont1', fg = "black", width = 20).place(x = 500, y = 180)
+signal_end = tk.Entry(root, width = 5, textvariable = end_column).place(x = 700, y = 180)
 
-tk.Label(root, text = "Accuracy obtained after validating the model with 20% of training data : ", font = 'customFont1', fg = "black", width = 40, wraplength = 250).grid(columnspan = 1, row = 3, column = 0)
+tk.Label(root, text = "Accuracy obtained after validating the model with 20% of training data : ", font = 'customFont1', fg = "black", width = 40, wraplength = 250).grid(columnspan = 1, row = 4, column = 0)
 string_variable = tk.StringVar()
-tk.Label(root, textvariable = string_variable).grid(columnspan = 1, row = 3, column = 1)
+tk.Label(root, textvariable = string_variable).grid(columnspan = 1, row = 4, column = 1)
 
 def main():
     global training_set1, training_set2, training_set3, validation_set1, validation_set2, validation_set3
     global folder_path, start_column, end_column, classifier, string_variable
     
-    content = folder_path.get()
-    if content == '':
+    try :
+        folder_path = folder_path.get()
+    except :
         tk.messagebox.showwarning("Folder Path empty","Please specify folder path!")
     
     try :
@@ -60,7 +71,6 @@ def main():
     except :
         tk.messagebox.showwarning("Ending Column empty", "Please specify ending column!")
     
-    path = content
     signal_width = range(start_column, end_column)
 
     object1_data = pd.DataFrame()
@@ -74,7 +84,7 @@ def main():
     validation_set1 = pd.DataFrame() 
     validation_set2 = pd.DataFrame() 
     validation_set3 = pd.DataFrame()   
-    for subdir, dirs, files in os.walk(path):
+    for subdir, dirs, files in os.walk(folder_path):
         for dir in dirs:
             subdir_path = os.path.join(subdir, dir)
             for subdir1, dirs1, files1 in os.walk(subdir_path):
@@ -220,42 +230,57 @@ def main():
     tk.messagebox.showinfo("Completed", "Training and Validating model is completed successfully")
     
 
-trainBtnTxt = tk.StringVar()
-trainBtn = tk.Button(root, textvariable = trainBtnTxt, command = lambda:main() ,font = 'customFont1', bg = "azure", fg = "black",height = 3, width = 15)
-trainBtnTxt.set("Train")
-trainBtn.grid(columnspan = 6, column = 0, row = 2)
+trainBtn = tk.Button(root, text = "Train", command = lambda:main() ,font = 'customFont1', bg = "azure", fg = "black",height = 3, width = 15)
+trainBtn.grid(columnspan = 6, column = 0, row = 3)
 
 #For Testing
-tk.Label(root, text = "Testing the Model", font = 'customFont1', fg = "black", bg = "sky blue", width = 15).grid(columnspan = 1, row = 4, column = 0)
+tk.Label(root, text = "Testing the Model", font = 'customFont1', fg = "black", bg = "sky blue", width = 15).grid(columnspan = 1, row = 5, column = 0)
 
+def browse_file() :
+    global file_path
+    filename = filedialog.askopenfilename()
+    file_path.set(filename)
+    
 file_path = tk.StringVar()  
-tk.Label(root, text = "Path for Testing the Model : ", font = 'customFont1', fg = "black", width = 20).grid(columnspan = 1, row = 5, column = 0)
-training_path = tk.Entry(root, width = 60, textvariable = file_path).grid(row = 5, column = 1)
+tk.Label(root, text = "Path for Testing the Model : ", font = 'customFont1', fg = "black", width = 20).grid(columnspan = 1, row = 6, column = 0)
+training_path = tk.Entry(root, width = 60, textvariable = file_path).grid(row = 6, column = 1)
 
+browseFileBtn = tk.Button(root, text = "Browse File", command = lambda:browse_file(), font = 'customFont1', bg = "azure", fg = "black", height = 2, width = 15)
+browseFileBtn.grid(row = 6, column = 2)
+
+def browse_outputFile() :
+    global file_output_path
+    outputfilename = filedialog.askdirectory()
+    filename = "/Output.xlsx"
+    file_output_path.set(outputfilename + filename)
+    
 file_output_path = tk.StringVar()
-tk.Label(root, text = "Path to save data file after testing : ", font = 'customFont1', fg = "black", width = 40).grid(columnspan = 1, row = 6, column = 0)
-training_path = tk.Entry(root, width = 60, textvariable = file_output_path).grid(row = 6, column = 1)
+tk.Label(root, text = "Path to save data file after testing : ", font = 'customFont1', fg = "black", width = 40).grid(columnspan = 1, row = 7, column = 0)
+training_path = tk.Entry(root, width = 60, textvariable = file_output_path).grid(row = 7, column = 1)
+
+browseOFileBtn = tk.Button(root, text = "Browse Output Folder", command = lambda:browse_outputFile(), font = 'customFont1', bg = "azure", fg = "black", height = 2, width = 15)
+browseOFileBtn.grid(row = 7, column = 2)
 
 def testing():
     global file_path, file_output_path
     
-    filepath = file_path.get()
-    if filepath == '':
+    try :
+        file_path = file_path.get()
+    except :
         tk.messagebox.showwarning("Testing file path empty", "Please specify file path for Testing!")
-        
-    fileoutput_path = file_output_path.get()
-    if fileoutput_path == '':
+       
+    try :    
+        file_output_path = file_output_path.get()
+    except :
         tk.messagebox.showwarning("Output file path empty", "Please specify file path for the Output file!")
         
     signal_width = range(start_column, end_column)
-    test_path = filepath
-    output_path = fileoutput_path
 
-    test_data = pd.read_excel(test_path, header = None, usecols = signal_width)
+    test_data = pd.read_excel(file_path, header = None, usecols = signal_width)
 
     y_test = classifier.predict(test_data.values)
 
-    workbook = openpyxl.load_workbook(test_path)
+    workbook = openpyxl.load_workbook(file_path)
     worksheet = workbook.worksheets[0]
     worksheet.insert_cols(3407)
     y = 1
@@ -264,18 +289,16 @@ def testing():
         cell_to_write.value = y_test[x]
         y += 1
 
-    workbook.save(output_path)
+    workbook.save(file_output_path)
     tk.messagebox.showinfo("Completed", "Testing is completed and Output file is generated")
     
-testBtnTxt = tk.StringVar()
-testBtn = tk.Button(root, textvariable = testBtnTxt, command = lambda:testing() ,font = 'customFont1', bg = "azure", fg = "black",height = 3, width = 15)
-testBtnTxt.set("Test")
-testBtn.grid(columnspan = 6, column = 0, row = 7)
+testBtn = tk.Button(root, text = "Test", command = lambda:testing() ,font = 'customFont1', bg = "azure", fg = "black",height = 3, width = 15)
+testBtn.grid(columnspan = 6, column = 0, row = 8)
 
 def Close():
     root.destroy()
   
 # Button for closing
-exit_button = tk.Button(root, text = "Exit", command = Close, height = 3, width = 15).grid(columnspan = 6, column = 3, row = 7)
+exit_button = tk.Button(root, text = "Exit", command = Close, height = 3, width = 15).grid(columnspan = 6, column = 3, row = 8)
 
 root.mainloop()
